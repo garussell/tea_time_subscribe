4f58c6fc3ed30cc2d5e602e355836a20afc92127
Allen Russell <garyrusselljr@gmail.com>
2023-10-31 10:02:21 -0600

    docs: add seeds and coverage and fixtures to gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Seed Customers
+10.times do
+  customer = Customer.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    address: Faker::Address.street_address
+  )
+
+  # Seed Subscriptions for each Customer
+  2.times do
+    customer.subscriptions.create(
+      title: Faker::Commerce.product_name,
+      price: Faker::Commerce.price,
+      status: Faker::Lorem.word,
+      frequency: Faker::Lorem.word
+    )
+  end
+end
+
+# Seed Teas for each Subscription
+Subscription.all.each do |subscription|
+  3.times do
+    subscription.teas.create(
+      title: Faker::Tea.variety,
+      description: Faker::Lorem.sentence,
+      temperature: Faker::Number.between(from: 70.0, to: 100.0),
+      brew_time: Faker::Number.between(from: 2, to: 5)
+    )
+  end
+end
